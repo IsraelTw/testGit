98a60fcb54f4dfc6e9f67edc64a45982df94f039
IsraelTw <azzzza19@gmail.com>
2022-11-14 12:59:35 +0200

    aa aaa aaa

--- handle-git.js
+++ handle-git.js
@@ -1,14 +1,20 @@
-import { execSync } from 'node:child_process';
+import { execSync, exec } from 'node:child_process';
 import inquirer from 'inquirer';
+import util from 'util';
+
+const exe = util.promisify(exec);
 
 (async () => {
     try {
         // await exe('git pull');
-        execSync('git add .');
+        // execSync('git add .');
+        await exe('git add .');
         const { commit } = await inquirer.prompt([{ type: 'input', message: 'enter commit', name: 'commit' }]);
-        console.log( commit);
-        execSync(`git commit -m "${commit}"`);
-        execSync(`git push`);
+        console.log(commit);
+        // execSync(`git commit -m "${commit}"`);
+        await exe(`git commit -m "${commit}"`);
+        // execSync(`git push`);
+        await exe(`git push`);
     }
     catch (err) {
         console.error(err);
