087904efcd5af5616fe198296be4b4c36a720a02
IsraelTw <azzzza19@gmail.com>
2022-11-14 13:28:57 +0200

    asas

--- handle-git.mjs
+++ handle-git.mjs
@@ -6,14 +6,18 @@ const exe = util.promisify(exec);
 
 (async () => {
     try {
+        const pull = await exe('git pull');
+        console.log(pull.stdout);
         const add = await exe('git add .');
+        console.log('git pull');
         console.log(add.stdout);
         const { commit } = await inquirer.prompt([{ type: 'input', message: 'enter commit', name: 'commit' }]);
-        const comm = await exe(`git commit -m "${commit}" && git push`);
-        console.log(comm);
+        const comm = await exe(`git commit -m "${commit}"`);
+        console.log(`git commit -m "${commit}"`);
         console.log(comm.stdout);
-        // const push = await exe(`git push`);
-        // console.log(push.stdout || push.stderr);
+        const push = await exe(`git push`);
+        console.log('git push');
+        console.log(push.stdout || push.stderr);
     }
     catch (err) {
         console.error(err.stdout || err.stderr || err);
